4b6ae130c68256f7a9ff7d01be9f3ef5100537b2
godstime01 <youngestkid01@gmail.com>
2023-01-04 16:05:46 -0800

    -

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-for index in range(97, 123):
-    print(f"{chr(index)}", end="")
+for char in range(26):
+    print("{:s}".format(chr(char + ord("a"))), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if (chr(i) == 'e' or chr(i) == 'q'):
-        continue
-    print(f"{chr(i)}", end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
