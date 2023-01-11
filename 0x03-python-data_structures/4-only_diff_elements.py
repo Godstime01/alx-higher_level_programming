35d796c3b0813a2997bd9667c30a1d5f8bd11cfc
godstime01 <youngestkid01@gmail.com>
2023-01-10 18:39:26 -0800

    file 4

--- 0x03-python-data_structures/4-only_diff_elements.py
+++ 0x03-python-data_structures/4-only_diff_elements.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+
+def new_in_list(my_list, idx, element):
+
+    if idx < 0 or idx > len(my_list) - 1: return my_list
+
+    my_list[idx] = element
+    return my_list
+    
+
+if __name__ == "__main__":
+    l1 = new_in_list([2,3,4,5,6], 3, 100)
+    print(l1)
 
