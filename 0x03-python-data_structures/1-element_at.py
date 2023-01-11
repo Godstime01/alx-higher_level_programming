dc0a96221d1b469781d9dfdc84b367eab3bb4b92
godstime01 <youngestkid01@gmail.com>
2023-01-10 18:30:34 -0800

    filee 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,3 +5,10 @@ def element_at(my_list, idx):
 
     if idx < 0 or idx > len(my_list): return None
     return my_list[idx]
+
+if __name__ == "__main__":
+    elem = element_at([1,2,3,4,6, 10], 5)
+    elem1 = element_at([1,2,3,4,6, 10], -1)
+    elem2 = element_at([1,2,3,4,6, 10], 10)
+    print(elem, elem1, elem2)
+
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,11 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    my_list.reverse()
-    for content in my_list:
-        print("{:d}".format(content))
+    if my_list:
+        my_list.reverse()
+        
+        for content in my_list:
+            print("{:d}".format(content))
 
 if __name__ == "__main__":
     print_reversed_list_integer([1,2,3,4,5,6])
