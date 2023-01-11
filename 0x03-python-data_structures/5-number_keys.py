68e6d4568a91bb3212e41eb74a484ce979556015
godstime01 <youngestkid01@gmail.com>
2023-01-10 19:07:42 -0800

    file 5

--- 0x03-python-data_structures/5-number_keys.py
+++ 0x03-python-data_structures/5-number_keys.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    return len(a_dictionary.keys())
+
+def no_c(my_string):
+    
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
+
+if __name__ == "__main__":
+   print(no_c('character'))
