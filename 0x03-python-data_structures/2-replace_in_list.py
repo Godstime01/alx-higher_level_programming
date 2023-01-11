520b7e72a5947c5c39c55f7cab172b21eb8551cd
godstime01 <youngestkid01@gmail.com>
2023-01-10 18:01:59 -0800

    file 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,3 +5,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) : return my_list
     my_list[idx] = element
     return my_list
+
+if __name__ == "__main__":
+    _ = replace_in_list([3, 5,67,19, 0], 0, 100)
+    print(_)
