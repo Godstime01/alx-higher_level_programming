dfa7b1b2303f900a48d25b5dae9d290a70edf534
Godstime01 <youngestkid01@gmail.com>
2023-06-07 19:17:09 -0700

    jj

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     for i in str:
         print('{}'.format(chr(ord(i) - 32) if 97 <= ord(i) <= 122 else i), end="")
-    print('\n')
+    print('\n') if str != ''
 
 
 if __name__ == '__main__':
