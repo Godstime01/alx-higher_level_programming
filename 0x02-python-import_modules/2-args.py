51ceeee3350688195fe44b3ea8bcb42b1c5153c6
Godstime01 <youngestkid01@gmail.com>
2023-06-10 17:23:10 -0700

    --

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import sys
 
-length_of_arg = len(sys.argv)
-if(length_of_arg < 2):
-    print('{} arguments.'.format(0))
-else:
-    print('{} {}:'.format(length_of_arg - 1, 'argument' if length_of_arg < 3 else 'arguments'))
+if __name__ == '__main__':
 
-    for i in range(1, length_of_arg):
-        print('{}: {}'.format(i, sys.argv[i]))
+    import sys
+
+    length_of_arg = len(sys.argv)
+    if(length_of_arg < 2):
+        print('{} arguments.'.format(0))
+    else:
+        print('{} {}:'.format(length_of_arg - 1, 'argument' if length_of_arg < 3 else 'arguments'))
+
+        for i in range(1, length_of_arg):
+            print('{}: {}'.format(i, sys.argv[i]))
